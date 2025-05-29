99597bf3db0a995b349568aa14d9f245d1f0f44c
Rajesh Sarkar <Rajeshsarkar@gmail.com>
2025-05-29 17:19:39 +0530

    all ok

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URI )
 
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
-app.get('/', (req, res) => res.send('🚀 API is running...'));
+app.get('/', (req, res) => res.send(' API is running...'));
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
