3a28faeb8c4b86659a06b93585d476907c4fcb2a
Rajesh Sarkar <Rajeshsarkar@gmail.com>
2025-05-29 13:55:21 +0530

    server is ok =

--- server.js
+++ server.js
@@ -11,10 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB Connected'));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log(' MongoDB Connected'))
+  .catch(err => console.error(' MongoDB connection error:', err));
 
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
@@ -22,4 +21,4 @@ app.use('/api/products', productRoutes);
 app.get('/', (req, res) => res.send('API is running...'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
