0a42d8e61b232e8e9fca1a6235ba202dad384e53
Rajesh Sarkar <Rajeshsarkar@gmail.com>
2025-05-29 15:45:14 +0530

    changes port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI )
   .then(() => console.log('✅ MongoDB Connected'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
@@ -19,5 +19,5 @@ app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.get('/', (req, res) => res.send('🚀 API is running...'));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
