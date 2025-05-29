b197ce06f010798fa14fed0635167a2ac506fd8d
Rajesh Sarkar <Rajeshsarkar@gmail.com>
2025-05-29 15:19:09 +0530

    minire changes

--- server.js
+++ server.js
@@ -5,20 +5,19 @@ const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 
-dotenv.config();
+dotenv.config(); 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log(' MongoDB Connected'))
-  .catch(err => console.error(' MongoDB connection error:', err));
+  .then(() => console.log('✅ MongoDB Connected'))
+  .catch(err => console.error('❌ MongoDB connection error:', err));
 
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
-
-app.get('/', (req, res) => res.send('API is running...'));
+app.get('/', (req, res) => res.send('🚀 API is running...'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
